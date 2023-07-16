99d56a23332666f4e68b0ae86c79bc116f72d67b
Vadym Bobyrenko <wadimcka@gmail.com>
2023-07-16 22:19:49 +0200

    task03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,16 @@
 var throttle = require('lodash.throttle');
 
 const formEl = document.querySelector('.feedback-form');
-
-// console.log(formEl);
 const FORM_KEY = 'feedback-form-state';
 
 formEl.addEventListener('input', throttle(onTargetElInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
-// refillInputsValue();
-
-let formValue = {};
+const formValue = {};
 
 function onTargetElInput(event) {
   formValue[event.target.name] = event.target.value;
   localStorage.setItem(FORM_KEY, JSON.stringify(formValue));
-  //   console.log(localStorage);
 }
 
 function onFormSubmit(event) {
@@ -29,12 +24,12 @@ function onFormSubmit(event) {
 }
 
 refillInputsValue();
-function refillInputsValue(event) {
+function refillInputsValue() {
   const formVulues = localStorage.getItem(FORM_KEY);
   console.log(formVulues);
-  parsedFormData = JSON.parse(formVulues);
+  const parsedFormData = JSON.parse(formVulues);
 
-  formData = new FormData(formEl);
+  const formData = new FormData(formEl);
   formData.forEach((value, name) => {
     if (parsedFormData.hasOwnProperty(name)) {
       formEl.elements[name].value = parsedFormData[name];

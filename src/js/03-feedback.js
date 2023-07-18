3f609ebf262f1dab3f141520fbd6b4bae47317d3
Vadym Bobyrenko <wadimcka@gmail.com>
2023-07-18 16:42:40 +0200

    task-03 fix-02

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,11 +6,18 @@ const FORM_KEY = 'feedback-form-state';
 formEl.addEventListener('input', throttle(onTargetElInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
-const formValue = {};
+let formValue = {};
+console.dir(formEl);
 
 function onTargetElInput(event) {
   formValue[event.target.name] = event.target.value;
-  localStorage.setItem(FORM_KEY, JSON.stringify(formValue));
+
+  try {
+    localStorage.setItem(FORM_KEY, JSON.stringify(formValue));
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
 }
 
 function onFormSubmit(event) {
@@ -18,22 +25,24 @@ function onFormSubmit(event) {
   if (formEl.email.value === '' || formEl.message.value === '') {
     return alert('Fill in the required fields');
   } else {
+    console.log(formValue);
     localStorage.removeItem(FORM_KEY);
     event.currentTarget.reset();
+    formValue = {};
   }
 }
 
 refillInputsValue();
 function refillInputsValue() {
   const formVulues = localStorage.getItem(FORM_KEY);
-
   try {
     const parsedFormData = JSON.parse(formVulues);
     const formData = new FormData(formEl);
-
     formData.forEach((value, name) => {
       if (parsedFormData.hasOwnProperty(name)) {
         formEl.elements[name].value = parsedFormData[name];
+        formValue[name] = parsedFormData[name];
+        console.dir(formValue);
       }
     });
   } catch (error) {
